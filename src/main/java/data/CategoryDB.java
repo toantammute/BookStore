99011630d0c2d6d40c329c811e242e980df1b7b6
Harris <harrispham.dev@gmail.com>
2023-11-24 12:39:04 +0700

    add new CategoryDB.java

---
+++ src/main/java/data/CategoryDB.java
@@ -0,0 +1,37 @@
+package data;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+
+public class CategoryDB {
+    // GENERATE ID FUNCTION
+    public String generateId() {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        try
+        {
+            TypedQuery<String> query = em.createQuery(
+                    "SELECT c.categoryID FROM Category c ORDER BY c.categoryID DESC", String.class);
+            query.setMaxResults(1);
+            String lastId = query.getSingleResult();
+            if (lastId != null) {
+                int number = Integer.parseInt(lastId.substring(4));
+                number++; // Tăng giá trị số lên 1
+                String newId = String.format("cate%04d", number);
+                return newId;
+            } else {
+                return "CATE0000";
+            }
+        }catch(Exception e)
+        {
+            System.out.print(e);
+            trans.rollback();
+            throw new RuntimeException("CREATE NEW ID FAIL", e);
+        }
+        finally {
+            em.close();
+        }
+    }
+}
